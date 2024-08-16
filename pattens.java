c064e98cd82254a743d8f8146e74c1005eef70e2
Rakshitson1410 <rakshitrsoni@gmail.com>
2024-08-17 01:05:49 +0530

    update

--- pattens.java
+++ pattens.java
@@ -1,5 +1,13 @@
 public class pattens {
     public static void main(String[] args) {
-        
+     
     }
+    static void patten1(int n){
+        for (int row = 0; row <n; row++) {
+            for (int i = 0; i < 10; i++) {
+                
+            }
+        }    
+    }
+
 }
