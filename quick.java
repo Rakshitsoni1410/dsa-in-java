82ba2d996cd2195b15fbdc30aa3591d4349ec87c
Rakshitson1410 <rakshitrsoni@gmail.com>
2024-12-20 12:35:19 +0530

    vfds

--- quick.java
+++ quick.java
@@ -1,3 +1,24 @@
 public class quick {
-    
-}
+    public static void main(String[] args) {
+        
+    }
+    static  void sort(int []nums,int low ,int high) {
+        if (low >=high) {
+            return;
+        }
+        int  s =low;
+        int e =high;
+        int m = s+(e-s)/2;
+        int pivot = nums[m];
+        while (s<=e ) {
+            while (nums[s]<pivot) {
+                s++;
+            }
+            while (nums[e]>pivot) {
+                e--;
+            }
+            if (s <=e) {
+                
+            }
+        }
+    }}
